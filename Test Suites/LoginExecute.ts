44931b94dc52a510d859e7b4cb384583074d51d5
mapolayom <mapolayom@10.92.4.91>
2018-10-25 11:55:37 -0500

    Inicio1

---
+++ Test Suites/LoginExecute.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>LoginExecute</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-10-25T11:53:39</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>4f8cf7e3-a882-4e05-867a-d8ba3f61f308</testSuiteGuid>
+   <testCaseLink>
+      <guid>93798765-d100-4e9e-a5cb-c8327eb872db</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
